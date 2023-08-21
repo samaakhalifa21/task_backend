05eaf2a329e7a2ad3e0d3218ebe32e0aecf54ae2
samaakhalifa21 <138895014+samaakhalifa21@users.noreply.github.com>
2023-08-21 19:33:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+'what output';
+1-10;
+2-"john";
+3-5;
+4-1,2,3,4,5;
+
+'write code';
+function addValues(num1,num2,num3,num4){
+    let sum =0;
+    sum=num1+num2+num3+num4;
+    return sum;
+}
+document.write (addValues(1,2,3,4));
---
+++ main.js
@@ -0,0 +1,46 @@
+function test(num1,num2){
+    return ((x == 50 || y == 50) || (x + y == 50));
+}
+
+console.log (test(50,50));
+
+
+
+
+
+
+function integer(x,y){
+    if ((x<0 && y>0)||(x>0&& y<0)){
+        return true
+    }
+}
+console.log (integer(2,50));
+
+
+
+
+
+
+
+
+function multiple(x){
+
+    if (x%5==0)
+    {
+        "x is multipule of 5";
+
+    }
+     else if (x%8==0){
+       return  "x is multipule of 5";
+     }
+
+else {
+    return false ;
+}
+
+}
+console.log (multiple(2));
+
+
+
+
